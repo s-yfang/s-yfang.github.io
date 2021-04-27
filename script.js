1deefc37ec701b7d7dc5d28d5a2855dea221d13e
Sheng <shengfang798@gmail.com>
2021-04-27 16:46:15 -0400

    Updated main page design

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 // script.js
 
+/*
+$(document).ready(function() {
+
+});
+
+// Javascipt only
+window.onload(function() {
+
+});
+
+var element = document.getElementById("name")
+
+// jQuery 
+// useful bc it makes it cross compatible
+// faster and shorter
+$(window).load(function() {
+
+})
+
+var elm = $("name")
+*/
